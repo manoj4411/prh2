eaa03c37da643b14b53a3ef2bbc2ad7b68517671
smk4411 <saimanojkumar63@gmail.com>
2024-06-19 11:20:50 +0530

    phr8

--- src/prh3.java
+++ src/prh3.java
@@ -1,6 +1,8 @@
 public class prh3 {
     public static void main(String[] args) {
         int a=40;
-        System.out.println(a=40);
+        short s1=35;
+        long l1=897;
+        System.out.println(s1=40);
     }
 }
